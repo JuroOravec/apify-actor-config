0346de481b66c394952e5f12f52739dca91b19ae
Juro Oravec <juraj.oravec.josefson@gmail.com>
2023-04-28 09:51:20 +0200

    refactor: fix cli command exec

--- src/cli/cli.ts
+++ src/cli/cli.ts
@@ -24,14 +24,14 @@ Example call:
   );
 
 program
-  .command('gen', 'generate actor.json config from a .js file')
+  .command('gen')
   .option('-c, --config <config-path>', 'path to actor config')
   .option(
     '-o, --out-dir [output-path]',
     'path to dir where the actor.json is exported. By default exports to "./.actor" if it exists, otherwise to "./"'
   )
   .option('-s, --silent', 'do not write log messages to stdout')
-  .action(async (str, options: CLIOptions) => {
+  .action(async (options: CLIOptions) => {
     await generate(options);
   });
 
